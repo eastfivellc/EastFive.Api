30e98913c2c3931dc5c0e53f11d8025bcda04266
joshcodes <josh@blackbarlabs.com>
2016-02-22 22:13:48 -0600

    added extension method to parse guid strings on resources

---
+++ BlackBarLabs.Api/Extensions/ResourceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BlackBarLabs.Api
+{
+    public static class ResourceExtensions
+    {
+        public static IEnumerable<Guid> ParseGuidString(this string guidString)
+        {
+            if (String.IsNullOrWhiteSpace(guidString))
+                return new Guid[] { };
+
+            var guids = guidString.Split(new char[','])
+                .Where(guidStringCandidate => { Guid g; return Guid.TryParse(guidStringCandidate, out g); })
+                .Select(guidStringCandidate => { Guid g; Guid.TryParse(guidStringCandidate, out g); return g; });
+            return guids;
+        }
+    }
+}
