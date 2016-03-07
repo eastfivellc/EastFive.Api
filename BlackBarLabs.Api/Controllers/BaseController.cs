1b1719bd65fbd1a73fb1ea0e2a76c8c04d77a44c
joshcodes <josh@blackbarlabs.com>
2016-03-07 17:56:56 -0600

    added controller to project ;-0

--- BlackBarLabs.Api/Controllers/BaseController.cs
+++ BlackBarLabs.Api/Controllers/BaseController.cs
@@ -6,43 +6,13 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 
-namespace OrderOwl.Api.Controllers
+namespace BlackBarLabs.Api.Controllers
 {
     public class BaseController : ApiController
     {
         protected BaseController()
         {
            
-        }
-
-        protected override void Initialize(HttpControllerContext controllerContext)
-        {
-            base.Initialize(controllerContext);
-
-            Func<BlackBarLabs.Web.ISendMailService> mailerServiceCreate =
-                () =>
-                {
-                    var username = ConfigurationManager.AppSettings["SendGrid.UserName"];
-                    var password  = ConfigurationManager.AppSettings["SendGrid.Password"];
-
-                    string testToEmailRedirect = null;
-                    var doEmailRedirect = ConfigurationManager.AppSettings["Test.ToEmail.DoRedirect"];
-                    if (doEmailRedirect == "true")
-                        testToEmailRedirect = ConfigurationManager.AppSettings["Test.ToEmail.Redirect"];
-
-                    var mailer = new BlackBarLabs.SendGrid.Mailer(username, password, testToEmailRedirect);
-                    return mailer;
-                };
-
-            controllerContext.Request.Properties.Add(
-                BlackBarLabs.Api.ServicePropertyDefinitions.MailService,
-                mailerServiceCreate);
-
-            Func<DateTime> fetchDateTimeUtc =
-                () => DateTime.UtcNow;
-            controllerContext.Request.Properties.Add(
-                BlackBarLabs.Api.ServicePropertyDefinitions.TimeService,
-                fetchDateTimeUtc);
         }
     }
 }
