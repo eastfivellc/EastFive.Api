c7f7923c08a7e1b93d9af6cf7c5eb1b57399c028
joshcodes <josh@blackbarlabs.com>
2016-03-08 13:13:54 -0600

    added resources Option and some validation extensions

---
+++ BlackBarLabs.Api/Resources/OptionsResource.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BlackBarLabs.Api.Resources
+{
+    public class Options
+    {
+        public object [] Get { get; set; }
+
+        public object[] Put { get; set; }
+
+        public object[] Post { get; set; }
+
+        public object[] Delete { get; set; }
+    }
+}
