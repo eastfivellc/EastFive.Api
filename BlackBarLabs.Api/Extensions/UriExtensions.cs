cfce27139a5286a5751e22776abc96645a1b7152
Keith Holloway <keith@eastfive.com>
2016-03-31 13:35:08 -0500

    Added new Uri.AddQuery extension

---
+++ BlackBarLabs.Api/Extensions/UriExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace BlackBarLabs.Api.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri AddQuery(this Uri uri, string name, string value)
+        {
+            var ub = new UriBuilder(uri);
+
+            // decodes urlencoded pairs from uri.Query to HttpValueCollection
+            var httpValueCollection = HttpUtility.ParseQueryString(uri.Query);
+
+            httpValueCollection.Add(name, value);
+
+            // urlencodes the whole HttpValueCollection
+            ub.Query = httpValueCollection.ToString();
+
+            return ub.Uri;
+        }
+    }
+}
