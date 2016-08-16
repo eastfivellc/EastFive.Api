dd4efb22d24fef272cc67c2afd88d4329ebfae7f
Josh <josh@eastfive.com>
2016-08-16 14:29:06 -0500

    added query resource types

---
+++ BlackBarLabs.Api/Resources/Queries/DateTimeQuery.cs
@@ -0,0 +1,103 @@
+﻿using BlackBarLabs.Core.Extensions;
+using BlackBarLabs.Web;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Api.Resources
+{
+    [TypeConverter(typeof(DateTimeQueryConverter))]
+    public class DateTimeQuery : TypeConverter
+    {
+        private string query;
+
+        public static implicit operator DateTimeQuery(string query)
+        {
+            if(default(string) == query)
+                return default(DateTimeQuery);
+            return new DateTimeQuery() { query = query };
+        }
+
+        internal TResult ParseInternal<TResult>(
+            Func<DateTime, DateTime, TResult> range,
+            Func<DateTime, TResult> specific,
+            Func<TResult> unparsable)
+        {
+            DateTime specificValue;
+            if (DateTime.TryParse(query, out specificValue))
+            {
+                return specific(specificValue);
+            }
+
+            DateTime start, end;
+            int offset = 1;
+            int offsetToggle = 1;
+            int index = query.Length / 2;
+            while (index > 0 && index < query.Length - 1)
+            {
+                var part1 = query.Substring(0, index);
+                var part2 = query.Substring(index);
+                if (DateTime.TryParse(part1, out start))
+                {
+                    if (DateTime.TryParse(part2, out end))
+                        return range(start, end);
+
+                    // Maybe there is a range character
+                    var separatorLength = 1;
+                    while (index + separatorLength < query.Length - 1)
+                    {
+                        part2 = query.Substring(index + separatorLength);
+                        if (DateTime.TryParse(part2, out end))
+                            return range(start, end);
+                        separatorLength++;
+                    }
+                }
+                index += (offset * offsetToggle);
+                offsetToggle *= -1;
+            }
+            return unparsable();
+        }
+    }
+
+    public static class DateTimeQueryExtensions
+    {
+        public static TResult Parse<TResult>(this DateTimeQuery query,
+            Func<DateTime, DateTime, TResult> range,
+            Func<DateTime, TResult> specific,
+            Func<TResult> empty,
+            Func<TResult> unparsable)
+        {
+            return query.HasValue(
+                (queryNotNull) => queryNotNull.ParseInternal(range, specific, unparsable),
+                () => empty());
+        }
+    }
+
+    class DateTimeQueryConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context,
+            CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                var valueString = value as string;
+                DateTimeQuery query = valueString;
+                return query;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ BlackBarLabs.Api/Resources/Queries/WebIdQuery.cs
@@ -0,0 +1,63 @@
+﻿using BlackBarLabs.Web;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Api.Resources
+{
+    [TypeConverter(typeof(WebIdQueryConverter))]
+    public class WebIdQuery
+    {
+        private string query;
+
+        public static implicit operator WebIdQuery(string query)
+        {
+            return new WebIdQuery() { query = query };
+        }
+
+        public static implicit operator WebIdQuery(Guid value)
+        {
+            return new WebIdQuery() { query = value.ToString() };
+        }
+
+        public TResult Parse<TResult>(
+            Func<IEnumerable<Guid>, TResult> multiple,
+            Func<TResult> unparsable)
+        {
+            Guid singleGuid;
+            if(Guid.TryParse(this.query, out singleGuid))
+            {
+                return multiple(singleGuid.ToEnumerable());
+            }
+            return unparsable();
+        }
+    }
+
+    class WebIdQueryConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context,
+            CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                var valueString = value as string;
+                WebIdQuery query = valueString;
+                return query;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
