0c2fe2c265b25662e84be7ac22f42ccb5526b18a
Josh <josh@eastfive.com>
2016-10-11 16:56:35 -0500

    added extension for converting CLR types to JSON types

---
+++ BlackBarLabs.Api/Extensions/TypeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Api
+{
+    public static class TypeExtensions
+    {
+        public static string AsJsonType(this Type type)
+        {
+            if (typeof(int) == type)
+                return "integer";
+            if (typeof(long) == type)
+                return "integer";
+            if (typeof(double) == type)
+                return "number";
+            if (typeof(float) == type)
+                return "number";
+            if (typeof(decimal) == type)
+                return "number";
+            if (typeof(bool) == type)
+                return "boolean";
+            if (typeof(string) == type)
+                return "string";
+
+            if (type.IsArray)
+                return "array";
+
+            return "object";
+        }
+    }
+}
