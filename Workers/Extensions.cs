015252750200be81acc805fbc8f036ccd37dc9e5
Josh <josh@eastfive.com>
2017-03-01 02:09:53 -0600

    more background task work

---
+++ Workers/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using BlackBarLabs.Api.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BlackBarLabs.Api
+{
+    public static class WorkerExtensions
+    {
+        public static string RegisterBackgroundTask(this HttpApplicationState application, Action backgroundTask)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new BackgroundWorker.DoWorkEventHandler(ListenerCallback);
+            worker.RunWorker(backgroundTask);
+            string taskName = Guid.NewGuid().ToString("N");
+            application[taskName] = worker;
+            return taskName;
+        }
+        
+        /// <summary> 
+        /// This operation listens to the queues without end
+        /// </summary> 
+        private static void ListenerCallback(ref int progress,
+            ref object _result, params object[] arguments)
+        {
+            var processor = (Action)arguments[0];
+            // Do the operation every 1 second wihout the end. 
+            while (true)
+            {
+                processor();
+            }
+        }
+    }
+}
