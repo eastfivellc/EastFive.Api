15c6adeea253f37edd7f218624ebae1ad984de1d
Josh <josh@eastfive.com>
2017-06-13 09:22:26 -0500

    added appsettings to standard location and added a new override to ParseAsync

---
+++ AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api
+{
+    public static class AppSettings
+    {
+        public const string ActorIdClaimType = "EastFive.Api.Security.AccountIdClaimType";
+        public const string ActorIdSuperAdmin = "EastFive.Api.Security.SiteAdminAccountId";
+        public const string SiteAdminAuthorization = "EastFive.Api.Security.SiteAdminAuthorization";
+    }
+}
