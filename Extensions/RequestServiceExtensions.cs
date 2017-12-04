eeeea8f2fa78a81250c4e3999163ddab6869ed72
Scott Turner <turner-scott@tds.net>
2017-12-04 15:09:52 -0600

    remove reference to EastFive.Api.Services

--- Extensions/RequestServiceExtensions.cs
+++ Extensions/RequestServiceExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using BlackBarLabs.Web;
-using BlackBarLabs.Extensions;
-using EastFive.Api.Services;
-using System.Threading.Tasks;
 using EastFive.Web.Services;
 
 namespace EastFive.Api
@@ -12,12 +8,12 @@ namespace EastFive.Api
     {
         public static Func<ISendMessageService> GetMailService(this HttpRequestMessage request)
         {
-            return Web.Services.ServiceConfiguration.SendMessageService;
+            return ServiceConfiguration.SendMessageService;
         }
 
         public static Func<ITimeService> GetDateTimeService(this HttpRequestMessage request)
         {
-            return Web.Services.ServiceConfiguration.TimeService;
+            return ServiceConfiguration.TimeService;
         }
     }
 }
