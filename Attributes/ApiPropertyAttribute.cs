592904b752798762e0bf142245a70be937ac6a3b
Josh Wingstrom <josh@eastfive.com>
2018-10-23 16:35:08 -0500

    added API property attribute

---
+++ Attributes/ApiPropertyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api
+{
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class ApiPropertyAttribute : System.Attribute
+    {
+        public ApiPropertyAttribute()
+        {
+        }
+        
+        private string propertyName;
+        public string PropertyName
+        {
+            get
+            {
+                return this.propertyName;
+            }
+            set
+            {
+                propertyName = value;
+            }
+        }
+    }
+}
