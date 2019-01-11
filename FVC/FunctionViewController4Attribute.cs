c8674b81695ef77841962bd0884099f3a04c5a4e
Josh Wingstrom <josh@eastfive.com>
2019-01-11 17:28:53 -0600

    a lot of new bindings

---
+++ FVC/FunctionViewController4Attribute.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api
+{
+    public class FunctionViewController4Attribute : FunctionViewControllerAttribute, IProvideSerialization
+    {
+        public HttpResponseMessage Serialize(HttpApplication httpApp, HttpRequestMessage request, ParameterInfo paramInfo, object obj)
+        {
+            var converter = httpApp.GetExtrudeConverter(request, request.GetUrlHelper());
+            var jsonObj = Newtonsoft.Json.JsonConvert.SerializeObject(obj, new JsonConverter[] { converter } );
+            var response = request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(jsonObj, Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ FVC/IProviderSerialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api
+{
+    public interface IProvideSerialization
+    {
+        HttpResponseMessage Serialize(HttpApplication httpApp, HttpRequestMessage request, ParameterInfo paramInfo, object obj);
+    }
+}
