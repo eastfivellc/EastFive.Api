f764d03b8ada1b1573f79944678c873682f7e746
Josh Wingstrom <josh@eastfive.com>
2019-01-24 08:49:51 -0600

    missed a file

---
+++ FVC/HttpActionDelegateAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api
+{
+    public class HttpActionDelegateAttribute : Attribute
+    {
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
