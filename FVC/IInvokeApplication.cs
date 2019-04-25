bc9755fe311369dbfaa3d516f51f59586c3ad379
Josh Wingstrom <josh@eastfive.com>
2019-04-25 16:17:31 -0500

    working on new pattern for calling api endpoints programatically

---
+++ FVC/IInvokeApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static EastFive.Api.HttpApplication;
+
+namespace EastFive.Api
+{
+    public interface IInvokeApplication
+    {
+        IDictionary<string, string> Headers { get; }
+
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
+
+        object CastResourceProperty(object value, Type propertyType);
+
+        void SetInstigator(Type type, InstigatorDelegate instigator, bool clear = false);
+
+        void SetInstigatorGeneric(Type type, InstigatorDelegateGeneric instigator, bool clear = true);
+    }
+}
