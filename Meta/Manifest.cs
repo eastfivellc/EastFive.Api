b1d601be8e1423b75168bc7f02133046cb925ba4
Joshua Wingstrom <josh@eastfive.com>
2019-12-08 11:59:17 -0600

    updating manifest capabilities

---
+++ Meta/Manifest.cs
@@ -0,0 +1,56 @@
+﻿using EastFive.Extensions;
+using EastFive.Linq;
+using EastFive.Reflection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api.Resources
+{
+    public class Manifest
+    {
+        public Manifest()
+        {
+
+        }
+
+        public Manifest(IEnumerable<Type> lookups,
+            HttpApplication httpApp)
+        {
+            this.Routes = lookups
+                .Where(type => type.ContainsAttributeInterface<IDocumentRoute>())
+                .Select(type => type.GetAttributesInterface<IDocumentRoute>().First().GetRoute(type, httpApp))
+                .OrderBy(route => route.Name)
+                .ToArray();
+        }
+
+        public Route[] Routes { get; set; }
+    }
+
+    public class Response
+    {
+        public Response(ParameterInfo paramInfo)
+        {
+            this.Name = paramInfo.Name;
+            this.StatusCode = System.Net.HttpStatusCode.OK;
+            //this.Example = "TODO: JSON serialize response type";
+            this.Headers = new KeyValuePair<string, string>[] { };
+        }
+
+        public Response()
+        {
+        }
+
+        public string Name { get; set; }
+
+        public System.Net.HttpStatusCode StatusCode { get; set; }
+
+        public string Example { get; set; }
+
+        public KeyValuePair<string, string>[] Headers { get; set; }
+    }
+}
--- Meta/Route.cs
+++ Meta/Route.cs
@@ -1,32 +1,17 @@
-﻿using EastFive.Extensions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Net.Http;
+using EastFive.Extensions;
 using EastFive.Linq;
 using EastFive.Reflection;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EastFive.Api.Resources
 {
-    public class Manifest
-    {
-        public Manifest(IEnumerable<Type> lookups,
-            HttpApplication httpApp)
-        {
-            this.Routes = lookups
-                .Where(type => type.ContainsAttributeInterface<IDocumentRoute>())
-                .Select(type => type.GetAttributesInterface<IDocumentRoute>().First().GetRoute(type, httpApp))
-                .OrderBy(route => route.Name)
-                .ToArray();
-        }
-
-        public Route[] Routes { get; set; }
-    }
-
     public class Route
     {
         public Route(string name, KeyValuePair<HttpMethod, MethodInfo[]>[] methods,
@@ -49,8 +34,8 @@ namespace EastFive.Api.Resources
                                         .Where(property => property.ContainsCustomAttribute<JsonPropertyAttribute>())
                                         .Select(member => new Property(member, httpApp))
                                         .ToArray();
-                                        //return new Property[] { };
-                                    },
+                                    //return new Property[] { };
+                                },
                                 () => new Property[] { });
                     },
                     () => new Property[] { });
@@ -82,29 +67,5 @@ namespace EastFive.Api.Resources
         public Method[] Methods { get; set; }
 
         public Property[] Properties { get; set; }
-    }
-
-
-    public class Response
-    {
-        public Response(ParameterInfo paramInfo)
-        {
-            this.Name = paramInfo.Name;
-            this.StatusCode = System.Net.HttpStatusCode.OK;
-            //this.Example = "TODO: JSON serialize response type";
-            this.Headers = new KeyValuePair<string, string>[] { };
-        }
-
-        public Response()
-        {
-        }
-
-        public string Name { get; set; }
-
-        public System.Net.HttpStatusCode StatusCode { get; set; }
-
-        public string Example { get; set; }
-
-        public KeyValuePair<string, string>[] Headers { get; set; }
     }
 }
