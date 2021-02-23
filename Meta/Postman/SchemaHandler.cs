af5700383a29f19c95c538f8db2ead12a4c39aff
Joshua Wingstrom <josh@eastfive.com>
2021-02-23 12:00:21 -0600

    bug fixes and new meta projects

---
+++ Meta/Postman/SchemaHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using EastFive.Api;
+using EastFive.Api.Core;
+using System.Linq;
+
+namespace EastFive.Api.Meta.Postman
+{
+    public class SchemaHandler
+    {
+        private readonly RequestDelegate continueAsync;
+        private HttpApplication httpApp;
+
+        public const string YamlMimeType = "text/yaml";
+
+        public SchemaHandler(RequestDelegate next, IApplication app)
+        {
+            this.continueAsync = next;
+            this.httpApp = (app as HttpApplication);
+        }
+
+        public async Task InvokeAsync(HttpContext context,
+               Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            if (!context.Request.GetAcceptTypes()
+                .Select(mt => mt.MediaType.ToLower())
+                .Contains(YamlMimeType))
+            {
+                await continueAsync(context);
+                return;
+            }
+
+            var lookups = httpApp.GetResources();
+            var manifest = new EastFive.Api.Resources.Manifest(lookups, httpApp);
+        }
+    }
+}
