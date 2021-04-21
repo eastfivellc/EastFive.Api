cee7e0f868098bc44266961b3fb3980d547f6fe2
Joshua Wingstrom <josh@eastfive.com>
2021-04-21 17:42:12 -0500

    fixed bug with double method matching and new MultipartAsyncResponse extension

--- Instigations/Responses/200s.cs
+++ Instigations/Responses/200s.cs
@@ -943,6 +943,15 @@ namespace EastFive.Api
             return UpdateResponse(parameterInfo, httpApp, request, response);
         }
     }
+    public static class MultipartAsyncResponseExtensions
+    {
+        public static IHttpResponse HttpResponse<TResource>(
+            this IEnumerableAsync<TResource> resources,
+            MultipartAsyncResponse<TResource> responseDelegate)
+        {
+            return responseDelegate(resources);
+        }
+    }
 
     [MultipartAcceptArrayResponse]
     public delegate IHttpResponse MultipartAcceptArrayResponse(IEnumerable<object> responses);
