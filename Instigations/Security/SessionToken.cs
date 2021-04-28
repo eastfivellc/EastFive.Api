8c7792ddfdd7be920966e4dd09f24cf6227f0323
Joshua Wingstrom <josh@eastfive.com>
2021-04-28 10:36:41 -0500

    added sessiontoken stuffs

--- Instigations/Security/SessionToken.cs
+++ Instigations/Security/SessionToken.cs
@@ -76,4 +76,73 @@ namespace EastFive.Api
                     .AsTask());
         }
     }
+
+    [SessionTokenMaybe]
+    public struct SessionTokenMaybe
+    {
+        public Guid? sessionId;
+        public Claim[] claims;
+        public Guid? accountIdMaybe;
+
+        public static Guid? GetClaimIdMaybe(IEnumerable<Claim> claims,
+            string claimType)
+        {
+            return claims.First(
+                (claim, next) =>
+                {
+                    if (String.Compare(claim.Type, claimType) != 0)
+                        return next();
+                    var accountId = Guid.Parse(claim.Value);
+                    return accountId;
+                },
+                () => default(Guid?));
+        }
+    }
+
+    public class SessionTokenMaybeAttribute : Attribute, IInstigatable
+    {
+        public Task<IHttpResponse> Instigate(IApplication httpApp,
+                IHttpRequest request, ParameterInfo parameterInfo,
+            Func<object, Task<IHttpResponse>> onSuccess)
+        {
+            return request.GetClaims(
+                (claimsEnumerable) =>
+                {
+                    var claims = claimsEnumerable.ToArray();
+                    return claims.GetAccountIdMaybe(
+                            request, Auth.ClaimEnableActorAttribute.Type,
+                        (accountIdMaybe) =>
+                        {
+                            var sessionIdClaimType = Auth.ClaimEnableSessionAttribute.Type;
+                            return claims.GetSessionIdAsync(
+                                            request, sessionIdClaimType,
+                                        (sessionId) =>
+                                        {
+                                            var token = new SessionTokenMaybe
+                                            {
+                                                accountIdMaybe = accountIdMaybe,
+                                                sessionId = sessionId,
+                                                claims = claims,
+                                            };
+                                            return onSuccess(token);
+                                        });
+                        });
+                },
+                () =>
+                {
+                    var token = new SessionTokenMaybe
+                    {
+                        accountIdMaybe = default,
+                        sessionId = default,
+                        claims = new Claim[] { },
+                    };
+                    return onSuccess(token);
+                },
+                (why) => request
+                    .CreateResponse(HttpStatusCode.Unauthorized)
+                    .AddReason(why)
+                    .AsTask());
+        }
+    }
+
 }
