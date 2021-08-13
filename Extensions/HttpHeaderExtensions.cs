f95bfdcb3434fbde78c13d4c022542d3ca7abc2c
Joshua Wingstrom <josh@eastfive.com>
2021-08-13 11:35:29 -0500

    null safe header accessors

--- Extensions/HttpHeaderExtensions.cs
+++ Extensions/HttpHeaderExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,5 +21,50 @@ namespace EastFive.Api
             return ((int)statusCode >= 200) && ((int)statusCode <= 299);
         }
 
+        public static string GetContentMediaTypeNullSafe(this HttpResponseMessage httpResponse)
+        {
+            if (httpResponse.IsDefaultOrNull())
+                return null;
+            return httpResponse.Content.GetContentMediaTypeNullSafe();
+        }
+
+        public static string GetContentMediaTypeNullSafe(this HttpContent httpContent)
+        {
+            if (httpContent.IsDefaultOrNull())
+                return null;
+            return httpContent.Headers.GetContentMediaTypeNullSafe();
+        }
+
+        public static string GetContentMediaTypeNullSafe(this HttpContentHeaders headers)
+        {
+            if (headers.IsDefaultNullOrEmpty())
+                return null;
+            if (headers.ContentType.IsDefaultOrNull())
+                return null;
+            return headers.ContentType.MediaType;
+        }
+
+        public static string GetFileNameNullSafe(this HttpResponseMessage httpResponse)
+        {
+            if (httpResponse.IsDefaultOrNull())
+                return null;
+            return httpResponse.Content.GetFileNameNullSafe();
+        }
+
+        public static string GetFileNameNullSafe(this HttpContent httpContent)
+        {
+            if (httpContent.IsDefaultOrNull())
+                return null;
+            return httpContent.Headers.GetFileNameNullSafe();
+        }
+
+        public static string GetFileNameNullSafe(this HttpContentHeaders headers)
+        {
+            if (headers.IsDefaultNullOrEmpty())
+                return null;
+            if (headers.ContentDisposition.IsDefaultOrNull())
+                return null;
+            return headers.ContentDisposition.FileName;
+        }
     }
 }
