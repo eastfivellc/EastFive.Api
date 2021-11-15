97b83ca472fcc4c77450e5e5104010d5164c44a9
Joshua Wingstrom <josh@eastfive.com>
2021-11-15 11:03:09 -0600

    starting to add postman

--- Meta/Postman/Resources/Collection/Collection.cs
+++ Meta/Postman/Resources/Collection/Collection.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using EastFive;
+using EastFive.Extensions;
+
 namespace EastFive.Api.Meta.Postman.Resources.Collection
 {
-    public class Collection
+
+
+    public class Collection : IReferenceable
     {
+        public Guid id => info._postman_id;
+
         public Info info { get; set; }
         public Item[] item { get; set; }
+
+
+        public static async Task<Collection> GetCollection(IRef<Collection> collectionRef)
+        {
+            return new Collection()
+            {
+
+            };
+        }
     }
 }
