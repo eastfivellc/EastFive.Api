0b87a1fcc649e0e8a9da263c13316d3daa6db91e
Scott Turner <scott@eastfive.com>
2021-12-15 12:34:07 -0600

    added PIIAdmin role claim

--- Auth/Claims/ClaimValues.cs
+++ Auth/Claims/ClaimValues.cs
@@ -3,9 +3,14 @@ namespace EastFive.Api.Auth
 {
     public static class ClaimValues
     {
+        public const string RoleType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role?id=";
+        public const string AccountType = "http://schemas.xmlsoap.org/ws/2009/09/identity/claims/actor";
+
         public static class Roles
         {
             public const string SuperAdmin = "superadmin";
+
+            public const string PIIAdmin = RoleType + "b253931513424afb83d5bff92498548b";
         }
     }
 }
