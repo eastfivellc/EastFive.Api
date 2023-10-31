56b06556898ce021cdd9b86c89b061bbf52153c6
Joshua Wingstrom <josh@eastfive.com>
2023-10-31 15:30:34 -0500

    Added ability to serialize IImplementRef

--- Serialization/Json/ExtrudeConvert.cs
+++ Serialization/Json/ExtrudeConvert.cs
@@ -38,6 +38,8 @@ namespace EastFive.Api.Serialization
         {
             if (objectType.IsSubClassOfGeneric(typeof(IRef<>)))
                 return true;
+            if (objectType.IsSubClassOfGeneric(typeof(IImplementRef<>)))
+                return true;
             if (objectType.IsSubClassOfGeneric(typeof(IRefs<>)))
                 return true;
             if (objectType.IsSubClassOfGeneric(typeof(IRefOptional<>)))
